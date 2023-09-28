ece940759a5a794c5ab850416d4817ff7d59a152
ShivamSikotra11 <sikotra756@gmail.com>
2023-09-28 20:52:23 +0530

    LoginDone

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template
 from app import app, db
 from app.models import *
 
-@app.route('/')
-def base():
-    return render_template('base.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-@app.route('/home')
+# @app.route('/home')
+# def home():
+#     return render_template('home.html')
+
+@app.route('/contact')
 def home():
-    return render_template('home.html')
+    return render_template('contact.html')
+
+@app.route('/login')
+
+def login():
+    return render_template('login.html')
+
+# @app.route('/')
+# def base():
+#     return render_template('base.html')
