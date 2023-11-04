f0033315d73c92a0bf4bbbcbd3140fc25639ea0e
ViralBiyawala <ui21cs66@iiitsurat.ac.in>
2023-11-04 18:57:00 +0530

    Internal to External JS

---
+++ app/static/positiveChart.js
@@ -0,0 +1,104 @@
+// Use JavaScript to fetch the dummy data for positive blood types
+fetch('/plot_positive_data')
+    .then(response => response.json())
+    .then(data => {
+        // Create the Highcharts column chart for positive blood types
+        var chart = Highcharts.chart('positiveChart', {
+            chart: {
+                type: 'line',
+                zoomType: 'x', // Enable x-axis zoom
+                panning: true, // Enable panning
+                panKey: 'shift' // Hold Shift key to enable panning
+            },
+            title: {
+                text: 'Positive Blood Types vs. Volume Inflow'
+            },
+            xAxis: {
+                categories: data.dates,
+                crosshair: true,
+            },
+            yAxis: {
+                title: {
+                    text: 'Volume Inflow'
+                }
+            },
+            series: [
+                {
+                    name: 'A+',
+                    data: data.Ap,
+                    visible: true, // Initially, this series is visible
+                    events: {
+                        click: function () {
+                            toggleVisibility(this); // Toggle visibility of the series
+                        }
+                    },
+                    marker: {
+                        enabled: false // Disable markers
+                    }
+                },
+                {
+                    name: 'B+',
+                    data: data.Bp,
+                    visible: true, // Initially, this series is visible
+                    events: {
+                        click: function () {
+                            toggleVisibility(this); // Toggle visibility of the series
+                        }
+                    },
+                    marker: {
+                        enabled: false // Disable markers
+                    }
+                },
+                {
+                    name: 'AB+',
+                    data: data.ABp,
+                    visible: true, // Initially, this series is visible
+                    events: {
+                        click: function () {
+                            toggleVisibility(this); // Toggle visibility of the series
+                        }
+                    },
+                    marker: {
+                        enabled: false // Disable markers
+                    }
+                },
+                {
+                    name: 'O+',
+                    data: data.Op,
+                    visible: true, // Initially, this series is visible
+                    events: {
+                        click: function () {
+                            toggleVisibility(this); // Toggle visibility of the series
+                        }
+                    },
+                    marker: {
+                        enabled: false // Disable markers
+                    }   
+                }
+            ]
+        });
+
+        // Function to toggle the visibility of a series
+        function toggleVisibility(clickedSeries) {
+            var tg = true;
+            chart.series.forEach(function (series) {
+                if (series.visible == false) {
+                    tg = false;
+                }
+            });
+            if (tg) {
+                chart.series.forEach(function (series) {
+                    if (series === clickedSeries) {
+                        series.setVisible(true, true);
+                    } else {
+                        series.setVisible(false, true);
+                    }
+                });
+            }
+            else {
+                chart.series.forEach(function (series) {
+                    series.setVisible(true, true);
+                });
+            }
+        }
+    });
