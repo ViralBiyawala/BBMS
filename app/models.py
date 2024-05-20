5e784807a356d927e0d84c786d8644081700cbdc
ViralBiyawala <ui21cs66@iiitsurat.ac.in>
2024-05-20 23:18:06 +0530

    Big Update

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy import func
+# from sqlalchemy import func
 
 class RDonor(UserMixin, db.Model):
     d_email_id = db.Column(db.String(100), primary_key=True)
